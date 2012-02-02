3cd712e46351477a53674df2e8ffe8719a165412
Alex Norman <x37v.alex@gmail.com>
2012-02-01 23:33:27 -0800

    added comments

--- main.c
+++ main.c
@@ -37,11 +37,17 @@ int main(void) {
       uint8_t rgb[3];
       uint8_t buf[4];
 
+      //grab 4 bytes from the uart.. its actually 8 bytes but they are ascii,
+      //converted in pairs to uint8_t
       ascii_uart_to_uint_buffer(buf, 4);
+
+      //if the first value matches our address or the broadcast address, work with it
       if (buf[0] == my_addr || buf[0] == broadcast_address) {
          hsv[0] = buf[1];
          hsv[1] = buf[2];
          hsv[2] = buf[3];
+
+         //convert to rgb and draw
          hsv_to_rgb(rgb, hsv);
          draw_rgb(rgb[0], rgb[1], rgb[2]);
       }
