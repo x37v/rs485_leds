0ffab505df87dd0cec87136948d522ed28b4451b
Alex Norman <x37v.alex@gmail.com>
2012-02-02 21:23:35 -0800

    working on clocking and interpolation

--- main.c
+++ main.c
@@ -18,11 +18,12 @@
  */
 
 #include <inttypes.h>
+#include <string.h>
 #include "hsv_to_rgb.h"
 #include "pauls_code.h"
 
 #ifndef MY_ADDRESS
-#define MY_ADDRESS 1
+#define MY_ADDRESS 0
 #endif
 
 //top 3 bits:
@@ -37,16 +38,24 @@ unsigned long time_ms = 0;
 
 typedef enum { ACTION_NONE, ACTION_ADDRESSED, ACTION_CLOCK } action_t;
 
+void draw_hsv(uint8_t hsv[3]);
+
+//dist is 0..255 where 255 is right at target and 0 is right at init
+void interp_hsv(uint8_t hsv_out[3], uint8_t hsv_init[3], uint8_t hsv_target[3], uint8_t dist);
+
 int main(void) {
    //init our pwm and uart inputs
    init_pwm_and_uart();
    draw_rgb(0,0,16);
    time_ms = 0;
+   unsigned long target_ms = 0;
+   uint8_t hsv_target[3] = {0, 0, 0};
+   uint8_t hsv_initial[3] = {0, 0, 0};
+   uint8_t hsv[3];
+   uint8_t start_packet;
+   uint16_t time_mul = 0;
 
    while (1) {
-      uint8_t hsv[3];
-      uint8_t rgb[3];
-      uint8_t start_packet;
       action_t action = ACTION_NONE;
 
       //loop until we get an start_packet packet, top bit is set
@@ -69,17 +78,62 @@ int main(void) {
       }
 
       if (action == ACTION_ADDRESSED) {
+         //reset time_ms
+         time_ms = 0;
+         
          //grab our colors, 7 bit
          for (uint8_t i = 0; i < 3; i++)
-            hsv[i] = (uart_getchar() << 1); //shift up because we're only getting 7 bit values
+            hsv_target[i] = (uart_getchar() << 1); //shift up because we're only getting 7 bit values
+         //grab our target time
+         target_ms = ((uint16_t)uart_getchar() << 7) | uart_getchar();
 
-         //convert to rgb and draw
-         hsv_to_rgb(rgb, hsv);
-         draw_rgb(rgb[0], rgb[1], rgb[2]);
+         if (target_ms == 0) {
+            //our initial is our target, copy and draw
+            memcpy(hsv_initial, hsv_target, 3 * sizeof(uint8_t));
+            memcpy(hsv, hsv_target, 3 * sizeof(uint8_t));
+            draw_hsv(hsv);
+
+            //XXX use different multipler depending on size of target_ms?
+            time_mul = (255 << 4) / target_ms;
+         } else {
+            memcpy(hsv_initial, hsv, 3 * sizeof(uint8_t));
+            draw_hsv(hsv_initial);
+         }
+
       } else {
          time_ms += (start_packet & 0x1f);
-         //XXX react to clock
+         if (time_ms < target_ms) {
+            //react to clock
+            //XXX use different multipler depending on size of target_ms?
+            uint8_t dist = (time_mul * time_ms) >> 4;
+            interp_hsv(hsv, hsv_initial, hsv_target, dist);
+            draw_hsv(hsv);
+         } else {
+            memcpy(hsv, hsv_target, 3 * sizeof(uint8_t));
+            draw_hsv(hsv);
+         }
       }
    }
 }
 
+void draw_hsv(uint8_t hsv[3]) {
+   uint8_t rgb[3];
+   hsv_to_rgb(rgb, hsv);
+   draw_rgb(rgb[0], rgb[1], rgb[2]);
+}
+
+uint8_t linear_interp(uint8_t x, uint16_t y0, uint16_t y1) {
+   return (y0 * (255 - x) + y1 * x) / 255;
+}
+
+void interp_hsv(uint8_t hsv_out[3], uint8_t hsv_init[3], uint8_t hsv_target[3], uint8_t dist) {
+   if (dist == 0) {
+      memcpy(hsv_out, hsv_init, 3 * sizeof(uint8_t));
+      return;
+   } else if (dist == 255) {
+      memcpy(hsv_out, hsv_target, 3 * sizeof(uint8_t));
+      return;
+   }
+   for (uint8_t i = 0; i < 3; i++)
+      hsv_out[i] = linear_interp(dist, hsv_init[i], hsv_target[i]);
+}
